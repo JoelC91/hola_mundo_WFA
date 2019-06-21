2b25003c9ccdfd095959d4dc6c0bc2a73e4aa500
JoelC91 <joel.carrizo.91@gmail.com>
2019-06-21 18:43:59 -0300

    initial commit

---
+++ principal.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace hola_mundo_WFA
+{
+    partial class principal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.btn_mensaje = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Location = new System.Drawing.Point(189, 82);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(75, 23);
+            this.btn_exit.TabIndex = 0;
+            this.btn_exit.Text = "Exit";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.Btn_exit_Click);
+            // 
+            // btn_mensaje
+            // 
+            this.btn_mensaje.Location = new System.Drawing.Point(13, 13);
+            this.btn_mensaje.Name = "btn_mensaje";
+            this.btn_mensaje.Size = new System.Drawing.Size(75, 23);
+            this.btn_mensaje.TabIndex = 1;
+            this.btn_mensaje.Text = "Mensaje";
+            this.btn_mensaje.UseVisualStyleBackColor = true;
+            this.btn_mensaje.Click += new System.EventHandler(this.Btn_mensaje_Click);
+            // 
+            // principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(276, 117);
+            this.Controls.Add(this.btn_mensaje);
+            this.Controls.Add(this.btn_exit);
+            this.Name = "principal";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.Button btn_mensaje;
+    }
+}
+
---
+++ principal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hola_mundo_WFA
+{
+    public partial class principal : Form
+    {
+        public principal()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+        }
+
+        private void Btn_exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Btn_mensaje_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola mundo");
+        }
+    }
+}
